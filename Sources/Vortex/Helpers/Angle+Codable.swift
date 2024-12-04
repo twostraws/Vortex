820bca89bb1bbd919fe0da8af4bf0df1316f63bb
Discoinferno <007gnail@gmail.com>
2024-12-04 15:11:34 +0000

    # Codable: Enabled automatic conformance of VortexSystem  to Codable by adding retroactive conformance of Codable to Angle. Removed redundant code.

---
+++ Sources/Vortex/Helpers/Angle+Codable.swift
@@ -0,0 +1,20 @@
+//
+//  Angle+Codable.swift
+//  Vortex
+// https://www.github.com/twostraws/Vortex
+// See LICENSE for license information.
+//
+
+import SwiftUI
+
+extension Angle: @retroactive Codable {
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        self = try Angle(radians: container.decode(Double.self))
+    }
+
+    public func encode(to encoder: any Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(self.radians)
+    }
+}
