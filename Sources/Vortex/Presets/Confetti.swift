e8d623847131e4d150c5f4fdbdcd1e81d01e8025
Paul Hudson <paul.hudson@gmail.com>
2025-05-02 12:04:50 +0100

    Switched over to map().

--- Sources/Vortex/Presets/Confetti.swift
+++ Sources/Vortex/Presets/Confetti.swift
@@ -30,29 +30,26 @@ extension VortexSystem {
     /// A customizable effect that creates confetti only when a burst is triggered.
     /// Relies on "confetti" tags being present – using `Rectangle`
     /// and `Circle` with frames of 16x16 works well.
-    /// Note: `.resolve(in: EnvironmentValues)` is only available in macOS 14.0 or later
     /// - Parameters:
-    ///   - colors: list of `SwiftUI.Color` to randomize in the confetti.
-    ///   - env: reference to `@Environment(\.self)`
+    ///   - colors: The array of `SwiftUI.Color` to randomize in the confetti.
+    ///   - environment: reference to `@Environment(\.self)`
     /// - Returns: an instance of `VortexSystem` to pass into the VortexView
     @available(macOS 14.0, *)
-    public static func accentedConfetti(
+    public static func confetti(
         colors: [SwiftUI.Color],
-        in env: EnvironmentValues
+        in environment: EnvironmentValues
     ) -> VortexSystem {
-        var vColors: [VortexSystem.Color] = []
-        for color in colors {
-            let components = color.resolve(in: env)
-            vColors
-                .append(
-                    Color(
-                        red: Double(components.red),
-                        green: Double(components.green),
-                        blue: Double(components.blue),
-                        opacity: Double(components.opacity)
-                    )
-                )
+        let resolvedColors = colors.map { color in
+            let components = color.resolve(in: environment)
+
+            return Color(
+                red: Double(components.red),
+                green: Double(components.green),
+                blue: Double(components.blue),
+                opacity: Double(components.opacity)
+            )
         }
+
         return VortexSystem(
             tags: ["confetti"],
             birthRate: 0,
@@ -62,7 +59,7 @@ extension VortexSystem {
             angleRange: .degrees(90),
             acceleration: [0, 1],
             angularSpeedVariation: [4, 4, 4],
-            colors: .random(vColors),
+            colors: .random(resolvedColors),
             size: 0.5,
             sizeVariation: 0.5
         )
